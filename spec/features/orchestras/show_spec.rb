70005caaa69d51f57ff95b994d1402937fa022c7
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-23 17:57:43 -0600

    test: add user story 2 test

--- spec/features/orchestras/show_spec.rb
+++ spec/features/orchestras/show_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe "Orchestra Show",type: :feature do
-  
+  describe "as a visitor" do
+    describe "when I visit '/orchestras/:id'" do
+      it "I see the orchestra with that id including the orchestra's attributes" do
+        orchestra_1 = Orchestra.create!(
+          name: "Colorado Symphony",
+          auditions_open: true,
+          year_established: 1989
+        )
+
+        visit "/orchestras/#{orchestra_1.id}"
+
+        expect(page).to have_content("#{orchestra_1.name}")
+        expect(page).to have_content("Auditions Open: #{orchestra_1.auditions_open}")
+        expect(page).to have_content("Year Established: #{orchestra_1.year_established}")
+      end
+    end
+  end
 end
