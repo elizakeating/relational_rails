8bee8790ae3d437d6cf06912394573fe80c0680e
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-23 20:42:47 -0600

    fix: fix musicians features index and spec test setup to account for addition of orchestra id

--- spec/features/musicians/index_spec.rb
+++ spec/features/musicians/index_spec.rb
@@ -4,15 +4,22 @@ RSpec.describe "Musicians Index",type: :feature do
   describe "as a visitor" do
     describe "when I visit '/musicians'" do
       it "I see each musician in the system including the musician's attributes" do
+        orchestra_1 = Orchestra.create!(
+          name: "Colorado Symphony",
+          auditions_open: true,
+          year_established: 1989
+        )
         musician_1 = Musician.create!(
           name: "Yumi Hwang-Williams",
           full_time: true,
-          years_involved: 23
+          years_involved: 23,
+          orchestra_id: orchestra_1.id
         )
         musician_2 = Musician.create!(
           name: "Dakota Cotugno",
           full_time: false,
-          years_involved: 1
+          years_involved: 1,
+          orchestra_id: orchestra_1.id
         )
 
         visit "/musicians"
--- spec/features/musicians/show_spec.rb
+++ spec/features/musicians/show_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe "Musician Show",type: :feature do
   describe "as a visitor" do
     describe "when I visit '/musicians/:id'" do
       it "I see the musician with that id including the musician's attributes" do
+        orchestra_1 = Orchestra.create!(
+          name: "Colorado Symphony",
+          auditions_open: true,
+          year_established: 1989
+        )
         musician_1 = Musician.create!(
           name: "Yumi Hwang-Williams",
           full_time: true,
-          years_involved: 23
+          years_involved: 23,
+          orchestra_id: orchestra_1.id
         )
 
         visit "/musicians/#{musician_1.id}"
