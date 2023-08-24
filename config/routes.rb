2853a80af0d1eb29e68cfc6bb4bffb3dd8d63c2d
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-23 20:25:58 -0600

    feat: add route for orchestra musicians index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get "/orchestras", to: "orchestras#index"
   get "/orchestras/:id", to: "orchestras#show"
+  get "/orchestras/:id/musicians", to: "orchestra_musicians#index"
 
   get "/musicians", to: "musicians#index"
   get "/musicians/:id", to: "musicians#show"
