682070940b9c417f30978cb8b579bf93880cbead
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-23 20:12:45 -0600

    feat: add association between orchestras and musicians

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_000848) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_021126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_000848) do
     t.integer "years_involved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "orchestra_id", null: false
+    t.index ["orchestra_id"], name: "index_musicians_on_orchestra_id"
   end
 
   create_table "orchestras", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_000848) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "musicians", "orchestras"
 end
