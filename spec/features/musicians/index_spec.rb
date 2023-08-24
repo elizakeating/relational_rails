dd7592522814b25dcfa800357427a284d504dd88
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-23 18:07:19 -0600

    test: add user story 3 test

--- spec/features/musicians/index_spec.rb
+++ spec/features/musicians/index_spec.rb
@@ -1,5 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "Musicians Index",type: :feature do
-  
+  describe "as a visitor" do
+    describe "when I visit '/musicians'" do
+      it "I see each musician in the system including the musician's attributes" do
+        musician_1 = Musician.create!(
+          name: "Yumi Hwang-Williams",
+          full_time: true,
+          years_involved: 23
+        )
+        musician_2 = Musician.create!(
+          name: "Dakota Cotugno",
+          full_time: false,
+          years_involved: 1
+        )
+
+        visit "/musicians"
+
+        expect(page).to have_content("#{musician_1.name}")
+        expect(page).to have_content("Full Time: #{musician_1.full_time}")
+        expect(page).to have_content("Years Involved: #{musician_1.years_involved}")
+        
+        expect(page).to have_content("#{musician_2.name}")
+        expect(page).to have_content("Full Time: #{musician_2.full_time}")
+        expect(page).to have_content("Years Involved: #{musician_2.years_involved}")
+      end
+    end
+  end
 end
