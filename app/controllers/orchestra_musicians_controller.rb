b340e18df12e946fec7f7cb01d257c4b74101442
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-27 13:51:26 -0600

    feat: update create action in orchestra musicians controller

--- app/controllers/orchestra_musicians_controller.rb
+++ app/controllers/orchestra_musicians_controller.rb
@@ -7,4 +7,17 @@ class OrchestraMusiciansController < ApplicationController
   def new
     @orchestra = Orchestra.find(params[:id])
   end
+
+  def create
+    orchestra = Orchestra.find(params[:id])
+    orchestra_musician = orchestra.musicians.create!({
+      name: params[:name],
+      full_time: params[:full_time],
+      years_involved: params[:years_involved]
+    })
+
+    orchestra_musician.save
+
+    redirect_to "/orchestras/#{orchestra.id}/musicians"
+  end
 end
