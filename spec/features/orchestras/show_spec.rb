e18ad58d4ddb3761af92e7f77a604d56874f493e
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-28 12:53:17 -0600

    test: add test for user story 19

--- spec/features/orchestras/show_spec.rb
+++ spec/features/orchestras/show_spec.rb
@@ -82,4 +82,63 @@ RSpec.describe "Orchestra Show",type: :feature do
       end
     end
   end
+
+  describe "as a visitor" do
+    describe "when I visit an orchestra show page" do
+      describe "then I see a link to delete the orchestra" do
+        describe "when I click the link 'delete orchestra'" do
+          it "a delete request is sent to '/orchestras/:id', the orchestra is deleted, all child records are deleted, and i am redirected to the orchestra index page where I no longer see this orchestra" do
+            orchestra_1 = Orchestra.create!(
+              name: "Colorado Symphony",
+              auditions_open: true,
+              year_established: 1989
+            )
+            orchestra_2 = Orchestra.create!(
+              name: "Chicago Symphony Orchestra",
+              auditions_open: false,
+              year_established: 1891
+            )
+            musician_1 = Musician.create!(
+              name: "Yumi Hwang-Williams",
+              full_time: true,
+              years_involved: 23,
+              orchestra_id: orchestra_1.id
+            )
+            musician_2 = Musician.create!(
+              name: "Dakota Cotugno",
+              full_time: false,
+              years_involved: 1,
+              orchestra_id: orchestra_1.id
+            )
+            musician_3 = Musician.create!(
+              name: "Paul Tyler",
+              full_time: true,
+              years_involved: 15,
+              orchestra_id: orchestra_2.id
+            )
+
+            visit "/orchestras/#{orchestra_1.id}"
+
+            expect(page).to have_content("Delete Orchestra")
+
+            click_link("Delete Orchestra")
+
+            expect(current_path).to eq("/orchestras")
+
+            expect(page).not_to have_content("Colorado Symphony")
+
+            visit "/musicians"
+
+            expect(page).not_to have_content("#{musician_1.name}")
+            expect(page).not_to have_content("Full Time: #{musician_1.full_time}")
+            expect(page).not_to have_content("Years Involved: #{musician_1.years_involved}")
+
+            expect(page).not_to have_content("#{musician_2.name}")
+            expect(page).not_to have_content("Full Time: #{musician_2.full_time}")
+            expect(page).not_to have_content("Years Involved: #{musician_2.years_involved}")
+          end
+        end
+      end
+    end
+  end
 end
