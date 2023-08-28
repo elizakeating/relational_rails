4140b79649b8bad2b4828d090f89faf99c3d3a57
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-28 00:01:50 -0600

    feat: rework test setup so there are no lingering entries

--- spec/features/orchestras/index_spec.rb
+++ spec/features/orchestras/index_spec.rb
@@ -31,38 +31,34 @@ RSpec.describe "Orchestras Index",type: :feature do
   
   describe "as a visitor" do
     describe "when I visit the orchestra index" do
-      orchestra_1 = Orchestra.create!(
-        name: "Colorado Symphony",
-        auditions_open: true,
-        year_established: 1989
-      )
-      sleep(1)
-      orchestra_2 = Orchestra.create!(
-        name: "Chicago Symphony Orchestra",
-        auditions_open: false,
-        year_established: 1891
-      )
-      sleep(1)
-      orchestra_3 = Orchestra.create!(
-        name: "Baltimore Symphony Orchestra",
-        auditions_open: true,
-        year_established: 1916
-      )
-
-      let(:created_at_1) { "<p>Created At: #{orchestra_1.created_at}</p>" }
-      let(:created_at_2) { "<p>Created At: #{orchestra_2.created_at}</p>" }
-      let(:created_at_3) { "<p>Created At: #{orchestra_3.created_at}</p>" }
-
       it "I see that records are ordered by most recently created first and next to each of the records I see when it was created" do
+        orchestra_1 = Orchestra.create!(
+          name: "Colorado Symphony",
+          auditions_open: true,
+          year_established: 1989
+        )
+        sleep(1)
+        orchestra_2 = Orchestra.create!(
+          name: "Chicago Symphony Orchestra",
+          auditions_open: false,
+          year_established: 1891
+        )
+        sleep(1)
+        orchestra_3 = Orchestra.create!(
+          name: "Baltimore Symphony Orchestra",
+          auditions_open: true,
+          year_established: 1916
+        )
+        
         visit "/orchestras"
 
         expect(page).to have_content("Created At: #{orchestra_1.created_at}")
         expect(page).to have_content("Created At: #{orchestra_2.created_at}")
         expect(page).to have_content("Created At: #{orchestra_3.created_at}")
 
-        expect(created_at_3).to appear_before(created_at_1)
-        expect(created_at_3).to appear_before(created_at_2)
-        expect(created_at_2).to appear_before(created_at_1)
+        expect("Created At: #{orchestra_3.created_at}").to appear_before("Created At: #{orchestra_1.created_at}")
+        expect("Created At: #{orchestra_3.created_at}").to appear_before("Created At: #{orchestra_2.created_at}")
+        expect("Created At: #{orchestra_2.created_at}").to appear_before("Created At: #{orchestra_1.created_at}")
       end
     end
   end
@@ -182,7 +178,7 @@ RSpec.describe "Orchestras Index",type: :feature do
             
             expect(page).to have_content("Update #{orchestra_1.name}")
             
-            click_link("Update #{orchestra_1.name}", match: :first)
+            click_link("Update #{orchestra_1.name}")
             
             expect(current_path).to eq("/orchestras/#{orchestra_1.id}/edit")
           end
