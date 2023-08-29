34130fc6f67b4c62a81fe6d29101782421da5549
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-28 22:33:41 -0600

    test: add orchestra musicians test for user story 23

--- spec/features/orchestras/musicians/index_spec.rb
+++ spec/features/orchestras/musicians/index_spec.rb
@@ -172,4 +172,43 @@ RSpec.describe "Orchestra Musicians Index",type: :feature do
       end
     end
   end
+
+  describe "as a visitor" do
+    describe "when I visit an orchestra's musicians index page" do
+      describe "next to every musician, I see a link to delete that musician" do
+        describe "when I click the link" do
+          it "I should taken to that orchestra's musicians index page where I no longer see that musician" do
+            orchestra_1 = Orchestra.create!(
+              name: "Colorado Symphony",
+              auditions_open: true,
+              year_established: 1989
+            )
+            musician_1 = Musician.create!(
+              name: "Yumi Hwang-Williams",
+              full_time: true,
+              years_involved: 23,
+              orchestra_id: orchestra_1.id
+            )
+            musician_2 = Musician.create!(
+              name: "Dakota Cotugno",
+              full_time: true,
+              years_involved: 1,
+              orchestra_id: orchestra_1.id
+            )
+
+            visit "/orchestras/#{orchestra_1.id}/musicians"
+
+            expect(page).to have_content("Delete #{musician_1.name}")
+            
+            click_link("Delete #{musician_1.name}")
+
+            expect(current_path).to eq("/musicians")
+
+            expect(page).not_to have_content("Yumi Hwang-Williams")
+            expect(page).not_to have_content("Years Involved: 23")
+          end
+        end
+      end
+    end
+  end
 end
