a3da07c1967b9d9b04470ffbeaa39fb9dd27336a
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-29 11:37:34 -0600

    feat: add sort method to orchestra model

--- app/models/orchestra.rb
+++ app/models/orchestra.rb
@@ -8,4 +8,15 @@ class Orchestra < ApplicationRecord
   def self.descending_created_at
     Orchestra.order(created_at: :desc)
   end
+
+  def sort(params)
+    musicians = self.musicians
+    if params[:sort] == "name"
+      musicians.order(:name)
+    elsif params[:sort] == "threshold"
+      musicians.where("years_involved > #{params[:threshold_value]}")
+    else
+      musicians
+    end
+  end
 end
